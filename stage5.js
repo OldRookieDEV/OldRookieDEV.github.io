f8e71d7d0d1ba5fdb9fa6b8ec5ecdf009824d592
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-09-26 12:39:03 +0900

    fixed

--- stage5.js
+++ stage5.js
@@ -478,7 +478,7 @@ var GameScene = new Phaser.Class({
 });
 
 var config = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
     scale: {
         mode: Phaser.Scale.FIT,
         parent: "raindrop",
